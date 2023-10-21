78709d8911656e1ddd9b289a08237b813627a9c5
Rishabh <rishabhpratap9719@h=gmail.com>
2023-10-21 16:12:37 +0530

    first comit

---
+++ src/Components/Home.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function Home() {
+  return (
+    <div className='container1'
+    style={{backgroundColor:'skyblue'}}
+    >
+      
+      Rablo.in
+    </div>
+
+  )
+}
+
+export default Home
